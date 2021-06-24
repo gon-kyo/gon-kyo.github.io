8dbf7e46a1d1cc83416ee77225ce002188197eb7
gon-kyo <dylanbest11@yahoo.com>
2021-06-24 09:25:39 -0500

    edsfd

--- connections.js
+++ connections.js
@@ -1,3 +1,35 @@
 function goHome(){
 	window.location.href="home.html";
 }
+
+function goBin(){
+	window.location.href="bin.html";
+}
+
+function goWheel(){
+	window.location.href="wheel.html";
+}
+
+function goAbout(){
+	window.location.href="about.html";
+}
+
+function goLinks(){
+	window.location.href="links.html";
+}
+
+function goArchive(){
+	window.location.href="archive.html";
+}
+
+function goFirst(){
+	window.location.href="entries/001.html";
+}
+
+function goMostRecent(){
+	window.location.href="entries/001.html";
+}
+
+function goSecret(){
+	window.location.href="secret.html";
+}
