0eae8c29fc4bfe5add471c1d62ee874fb2728779
gon-kyo <dylanbest11@yahoo.com>
2021-06-24 10:29:09 -0500

    dfsfhjk

---
+++ entries/entrynexus.js
@@ -0,0 +1,39 @@
+function goHome(){
+	window.location.href="../home.html";
+}
+
+function goBin(){
+	window.location.href="../bin.html";
+}
+
+function goWheel(){
+	window.location.href="../wheel.html";
+}
+
+function goAbout(){
+	window.location.href="../about.html";
+}
+
+function goLinks(){
+	window.location.href="../links.html";
+}
+
+function goAbout(){
+	window.location.href="../about.html";
+}
+
+function goArchive(){
+	window.location.href="../archive.html";
+}
+
+function goFirst(){
+	window.location.href="001.html";
+}
+
+function goMostRecent(){
+	window.location.href="001.html";
+}
+
+function goSecret(){
+	window.location.href="../secret.html";
+}
